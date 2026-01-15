e88b1ec75ca34695d687aa26f8b29b59510c92b7
266-266 <162500840+266-266@users.noreply.github.com>
2026-01-15 18:59:05 +0000

    Create JwtUtils.java

---
+++ security/JwtUtils.java
@@ -0,0 +1,31 @@
+package com.gigiTicket.backend.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private String jwtSecret = "maCleSecreteTresLongueEtSecuriseePourLeProjetUCAO";
+    private int jwtExpirationMs = 86400000; // 24 heures
+
+    // Méthode pour générer le Token après le login
+    public String generateJwtToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    // Méthode pour vérifier si un Token est encore valide
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
