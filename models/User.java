31f4f433c476f59b822fe4285baccb84f1900a32
266-266 <162500840+266-266@users.noreply.github.com>
2026-01-15 18:47:29 +0000

    Create User.java

---
+++ models/User.java
@@ -0,0 +1,23 @@
+package com.gigiTicket.backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // Sera crypté via BCrypt [cite: 47]
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // Gestion des rôles [cite: 40]
+}
